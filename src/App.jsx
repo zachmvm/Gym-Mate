a7231d58839ee67d2bea24d7bf0e428d88dc402f
Zach <zachmvm@gmail.com>
2024-03-19 15:03:39 -0400

    customize entry page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Workout from './components/Workout'
 function App() {
 
   return (
-    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to slate-950 text-white text-sm sm:text-base">
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
       <Generator />
       <Workout />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-        <p>IT'S TIME TO GET SWOLE</p>
-        <h1>Swole<span>normous</span></h1>
-        <p></p>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto'>
+        <div className='flex flex-col gap-4'>
+        <p>IT'S TIME TO GET </p>
+        <h1 className='uppercase fotn-bold text-5xl sm:text-5xl md:text-6xl lg:text-7xl'>
+        Swole<span className='text-blue-400'>normous</span></h1>
+        </div>
+
+        <p className='text-sm md:text-base font-light'>I hereby acknowdlege that I may become 
+        <span className='text-blue-400 font-medium'>unbelievably swole</span> and accept 
+        all risks of becoming the local <span className='text-blue-400 font-medium'> mass montrosity</span>
+         unable to fit through doors.
+        </p>
+        
+        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-300'>
+          <p>Accept & Begin</p>
+        </button>
     </div>
   )
 }
