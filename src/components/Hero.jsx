75bdce0448e4ed29d7404a64996112dc294ee82d
Zach <zachmvm@gmail.com>
2024-03-18 21:47:53 -0400

    add some components / fix css

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <p>IT'S TIME TO GET SWOLE</p>
+        <h1>Swole<span>normous</span></h1>
+        <p></p>
+    </div>
+  )
+}
