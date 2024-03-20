0adc5844339693c97c99cfb9dd16345b13456c12
Zach <zachmvm@gmail.com>
2024-03-20 19:52:28 -0400

    change color scheme

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
-import { WORKOUTS } from '../utils/swoldier'
+import { SCHEMES, WORKOUTS } from '../utils/swoldier'
 
 function Header(props) {
   const { index, title, description } = props
@@ -17,6 +17,10 @@ function Header(props) {
 
 export default function Generator() {
   const [showModal, setShowModal] = useState(false)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goals, setGoals] = useState('strength_power')
+
 
   function toggleModal() {
     setShowModal(!showModal)
@@ -28,25 +32,44 @@ export default function Generator() {
       <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
         {Object.keys(WORKOUTS).map((type, typeIndex) => {
           return (
-            <button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-2 rounded-lg' key={typeIndex}>
+            <button className='bg-slate-950 border border-green-400 duration-200 hover:border-green-600 py-2 rounded-lg' key={typeIndex}>
               <p className='capitalize'>{type.replaceAll('_','.')}</p>
             </button>
           )
 
         })}
       </div>
+
       {/* Create 2nd header */}
 
       <Header index={'02'} title={'Lock on targets'} description={'Select the muscles you want to flex.'} />
-      <div className='bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col'>
+      <div className='bg-slate-950 border border-solid border-green-400 rounded-lg flex flex-col'>
         <button onClick={toggleModal} className='relative p-3 flex items-center justify-center'>
           <p>Select muscle groups</p>
-          <i className="fa-solid absolute right-3 top-1/2 <translate-y-1/2 fa-angles-down"></i>
+          <i className="fa-solid absolute right-3 top-1/2 <translate-y-1/2 fa-arrow-turn-down"></i>
         </button>
         {showModal && (
           <div>modal</div>
         ) }
       </div>
+
+      {/* Create 3rd header */}
+
+      <Header index={'03'} title={'Become a veteran'} description={'Select your ultimate objective.'} />
+      <div className='grid grid-cols-3 gap-4'>
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button className='bg-slate-950 border border-green-400 duration-200 hover:border-green-600 py-2 rounded-lg' key={schemeIndex}>
+              <p className='capitalize'>{scheme.replaceAll('_','.')}</p>
+            </button>
+          )
+
+        })}
+      </div>
+          
+
+
+
     </SectionWrapper>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,16 @@ export default function Hero() {
         <div className='flex flex-col gap-4'>
         <p>IT'S TIME TO GET </p>
         <h1 className='uppercase fotn-bold text-5xl sm:text-5xl md:text-6xl lg:text-7xl'>
-        Swole<span className='text-blue-400'>normous</span></h1>
+        Soldier<span className='text-green-400'>Strong</span></h1>
         </div>
 
         <p className='text-sm md:text-base font-light'>I hereby acknowdlege that I may become 
-        <span className='text-blue-400 font-medium'>unbelievably swole</span> and accept 
-        all risks of becoming the local <span className='text-blue-400 font-medium'> mass montrosity</span>
+        <span className='text-green-400 font-medium'> unbelievably ripped</span> and accept 
+        all risks of becoming the biggest <span className='text-green-400 font-medium'> guy/gal in the gym </span>
          unable to fit through doors.
         </p>
         
-        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-300'>
+        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-green-400 border-solid greenShadow duration-300'>
           <p>Accept & Begin</p>
         </button>
     </div>
